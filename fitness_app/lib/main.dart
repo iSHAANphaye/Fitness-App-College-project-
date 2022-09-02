faf3c82c16e4fa36d6737d3d56689d35e3ad66a3
Ishaan Phaye <ishaanphaye@gmail.com>
2022-09-02 09:05:17 +0530

    Fitness App

---
+++ fitness_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:fitness_app/pages/homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
